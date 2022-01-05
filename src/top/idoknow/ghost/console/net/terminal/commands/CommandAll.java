af2fa332074be97e60a5d96ea83390b8d8f9c723
Rock Chin <1010553892@qq.com>
2022-01-05 17:30:29 +0800

    feat(!all): send command to all unfocused slave

--- src/top/idoknow/ghost/console/net/terminal/commands/CommandAll.java
+++ src/top/idoknow/ghost/console/net/terminal/commands/CommandAll.java
@@ -1,14 +1,17 @@
 package top.idoknow.ghost.console.net.terminal.commands;
 
+import top.idoknow.ghost.console.ioutil.log.LogMgr;
 import top.idoknow.ghost.console.net.protocol.AbstractCommand;
 import top.idoknow.ghost.console.net.protocol.AbstractHandler;
+import top.idoknow.ghost.console.net.slave.SlaveAcceptor;
+import top.idoknow.ghost.console.net.slave.SlaveHandler;
+import top.idoknow.ghost.console.net.terminal.TerminalAcceptor;
+import top.idoknow.ghost.console.net.terminal.TerminalHandler;
 import top.idoknow.ghost.console.net.terminal.UnauthorizedSessionException;
 import top.idoknow.ghost.console.subject.Subject;
 
 /**
- * Send message to all slaves.
- * This command is supposed to be expired,
- * but I have not found an elegant way to impl such function to replace "!all" yet.
+ * Send message to all unfocused slaves.
  * @author Rock Chin
  */
 public class CommandAll extends AbstractCommand {
@@ -17,6 +20,11 @@ public class CommandAll extends AbstractCommand {
         if (handler.getSubject().getIdentity()== Subject.UNDEFINED){
             throw new UnauthorizedSessionException("unauthorized terminal session.");
         }
-        //TODO find another good way
+        for (SlaveHandler slaveHandler:SlaveAcceptor.slaveHandlers){
+            if (slaveHandler.getPeerTerminal()==null||slaveHandler.getPeerTerminal()== handler)
+                slaveHandler.getDataProxy().appendMsg(rawData.substring(5));
+        }
+        LogMgr.logMessage(handler.getSubject(),"All"," Issued command on all unfocused slaves:"+rawData.substring(5));
+
     }
 }
